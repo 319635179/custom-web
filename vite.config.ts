c6c451092b65dd027aeb955e5f592e97b82833e9
wangzh49256 <wangzh49256@hundsun.com>
2024-09-21 17:08:03 +0800

    配置更新

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: "8120",
+    port: 8120,
     open: true,
   },
   plugins: [
